2cf3b9d180c9deb9d2bbdac23ea38033acffc4d3
EsioFreitas <esio.gustavo@gmail.com>
2021-04-28 19:55:58 -0300

    upgrade main
    Co-authored-by: MarcosNBJ <marcosnery.comp@gmail.com>

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from pade.misc.utility import start_loop
 from pade.acl.aid import AID
 from sys import argv
 
-if __name__ == '__main__':
-    agents_per_process = 3
-    c = 0
-    agents = list()
-    for i in range(agents_per_process):
-        port = int(argv[1]) + c
-        agent_name = 'agent_hello_{}@localhost:{}'.format(port, port)
-        agente_hello = AgenteHelloWorld(AID(name=agent_name))
-        agents.append(agente_hello)
-        c += 1000
 
-    start_loop(agents)
+def main():
+  agents_per_process = 3
+  c = 0
+  agents = list()
+  for i in range(agents_per_process):
+      port = int(argv[1]) + c
+      agent_name = 'agent_hello_{}@localhost:{}'.format(port, port)
+      agente_hello = AgenteHelloWorld(AID(name=agent_name))
+      agents.append(agente_hello)
+      c += 1000
+
+  start_loop(agents)
+
+if __name__ == '__main__':
+  main()
