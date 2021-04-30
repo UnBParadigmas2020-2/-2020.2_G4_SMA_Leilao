356851762fa7d5387888c1164dda2b52800995f8
Marcos <marcosnery.comp@gmail.com>
2021-04-30 11:59:11 -0300

    iniciando multiplos compradores e nomeando agentes

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from agentes.agente_comprador import AgenteComprador
+from agentes.agente_leiloeiro import AgenteLeiloeiro
 from pade.misc.utility import start_loop
 from pade.acl.aid import AID
 from sys import argv
 from random import *
-
+from pade.acl.messages import ACLMessage
+from pade.acl.filters import Filter
 
 
 def main():
-  agents_per_process = 3
-  c = 0
+  f = Filter()
+  f.performative = ACLMessage.INFORM
   agents = list()
-  for i in range(agents_per_process):
-      port = int(argv[1]) + c
-      agent_name = 'agent_{}'.format(port)
-      agent_dinheiro = randint(100,1000)
-      agente_comprador = AgenteComprador(AID(name=agent_name), agent_dinheiro)
+  port = int(argv[1]) 
+  agent_dinheiro = randint(100,1000)
+  agente_leiloeiro = AgenteLeiloeiro(
+      AID(name=f'leiloeiro@localhost:{port}'),f)
+  agents.append(agente_leiloeiro)  
+  port += 1
+  numero_de_compradores=3
+  for i in range(numero_de_compradores):
+      agente_comprador = AgenteComprador(AID(name=f'comprador_{i}@localhost:{port+i}'), f, agent_dinheiro)
       agents.append(agente_comprador)
-      c += 1000
-
   start_loop(agents)
 
 if __name__ == '__main__':
